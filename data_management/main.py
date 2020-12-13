e31d9300d4e946252cf6f79f8a45c9066d4725d7
mahns <mahns1201@gmail.com>
2020-12-14 07:43:46 +0900

    [fix] modify time

--- data_management/main.py
+++ data_management/main.py
@@ -8,15 +8,15 @@ def main():
     while True:
         from_arduino.make_file()
         print('update local txt file')
-        time.sleep(1)
+        time.sleep(5)
 
         to_firebase.upload_to_realtime()
         print("upload to realtime database")
-        time.sleep(1)
+        time.sleep(5)
 
         to_firebase.upload_to_firestore()
         print('upload to firestore')
         print(split * 105)
-        time.sleep(3)
+        time.sleep(300)
 
 main()
